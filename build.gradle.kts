4d08a0c2be59bda8ff0afe476970be972a94f7e7
rajroyal4545-design <rajroyal4545@gmail.com>
2026-09-19 09:34:38 +0530

    Add Google Services plugin to build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,5 +2,6 @@ plugins {
     id("com.android.application") version "9.1.1" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.2.10" apply false
     id("com.google.devtools.ksp") version "2.3.5" apply false
+    id("com.google.gms.google-services") version "4.5.0" apply false
     id("io.github.takahirom.roborazzi") version "1.59.0" apply false
 }
